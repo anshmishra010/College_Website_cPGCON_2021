b96e483cb44b4b03907c1c3a59e7597e9bed61ee
Pratik Patil <pratikap41@gmail.com>
2021-01-11 14:50:53 +0530

    Added Static assets logos

--- college_website/college_website/settings.py
+++ college_website/college_website/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'college_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+                os.path.join(BASE_DIR, "templets")
+             ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
